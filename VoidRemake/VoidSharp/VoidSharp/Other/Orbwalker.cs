a148441085fd0c2bc8a689cee7f0b81d9df71dcb
Šimon Novák <82063567+sajmonekk191@users.noreply.github.com>
2022-01-12 06:02:44 +0100

    Add files via upload

---
+++ VoidRemake/VoidSharp/VoidSharp/Other/Orbwalker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace VoidSharp.Other
+{
+    internal class Orbwalker
+    {
+        private static int LastAATick;
+        private static int LastMoveCommandT;
+        private static Point LastMovePoint;
+        private static readonly Random rnd = new Random();
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+        [DllImport("user32.dll")]
+        public static extern byte VkKeyScan(char ch);
+
+        private static int GetAttackWindup()
+        {
+            float champWindupPercent = CatHackGUIChampions.getWindupPercent();
+
+            int calc = (int)((1 / LocalPlayer.GetAttackSpeed() * 1000) * champWindupPercent);
+
+            return calc;
+        }
+
+        private static int GetAttackDelay()
+        {
+            return (int)(1000.0f / LocalPlayer.GetAttackSpeed());
+        }
+
+        private static bool CanAttack()
+        {
+            return LastAATick + GetAttackDelay() + SaveUserPing.getUserPing() / 2 < Environment.TickCount;
+        }
+
+        private static bool CanMove(float extraWindup)
+        {
+            if (CatHackGUIChampions.getSelectedChampion() == "Kalista")
+            {
+                return true;
+            }
+            else
+            {
+                return LastMoveCommandT < Environment.TickCount + extraWindup;
+            }
+        }    
+        public static void OrbWalkEnemy()
+        {
+            int orbwalkKey = VkKeyScan(CatHackGUIPlayer.getOrbwalkKey());
+            short keyStateTemp = GetAsyncKeyState(orbwalkKey);
+            bool keyIsPressed = ((keyStateTemp >> 15) & 0x0001) == 0x0001;
+
+            if (keyIsPressed && CatHackGUIOrb.getOrbCheck())
+            {
+                Point enemyPos = modules.ChampPosition.GetEnemyPosition();
+
+                if (CanAttack() && enemyPos != Point.Empty)
+                {
+                    LastMovePoint = Cursor.Position;
+                    KeyMouseHandler.IssueOrder(OrderEnum.AttackUnit, enemyPos);
+
+                    System.Threading.Thread.Sleep(5);
+
+                    LastAATick = Environment.TickCount + 3;
+                    LastMoveCommandT = Environment.TickCount + GetAttackWindup() + 3;
+
+                    KeyMouseHandler.IssueOrder(OrderEnum.MoveMouse, LastMovePoint);
+                }
+                else if (CanMove(CatHackGUIOrb.getExtraWindup()))
+                {
+                    KeyMouseHandler.IssueOrder(OrderEnum.RightClick);
+
+                    LastMovePoint = Cursor.Position;
+                    LastMoveCommandT = Environment.TickCount + rnd.Next(50, 80);
+                }
+                else
+                {
+                    if (enemyPos == Point.Empty)
+                    {
+                        LastMovePoint = Cursor.Position;
+                    }
+                    KeyMouseHandler.IssueOrder(OrderEnum.RightClick);
+                }
+                System.Diagnostics.Debug.WriteLine("Ping: " + SaveUserPing.getUserPing() + " | " + "Extra Windup: " + CatHackGUIOrb.getExtraWindup() + " | " + "Windup Percent: " + CatHackGUIChampions.getWindupPercent() + " | " + "Attack Delay: " + GetAttackDelay());
+            }
+        }
+}
---
+++ VoidRemake/VoidSharp/VoidSharp/Scripts/Orbwalker.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace VoidSharp.Scripts
+{
+    partial class Orbwalker
+    {
+        /// <summary> 
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód vygenerovaný pomocí Návrháře komponent
+
+        /// <summary> 
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Orbwalker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Orbwalker";
+            this.Size = new System.Drawing.Size(386, 253);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
