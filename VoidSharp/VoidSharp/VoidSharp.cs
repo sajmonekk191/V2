03ccbfcdcabe08a4b5b0f70fcbab4204bac8ecf5
Šimon Novák <82063567+sajmonekk191@users.noreply.github.com>
2022-01-12 13:23:41 +0100

    Add files via upload

---
+++ VoidSharp/VoidSharp/VoidSharp.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace VoidSharp
+{
+    public partial class VoidSharp : Form
+    {
+        Point mousedownpoint = Point.Empty;
+        public VoidSharp()
+        {
+            InitializeComponent();
+            this.Size = new Size(325, 160);
+            this.BackgroundImage = Properties.Resources.splashscreen;
+            loadinglabel.Location = new Point(117, 111);
+            progressBar1.Location = new Point(12, 138);
+            ExitButton.BackColor = Color.FromArgb(127, 37, 37);
+            MinimizeButton.BackColor = Color.FromArgb(108, 116, 116);
+            generaluser1.Visible = false;
+            LogoPic.Visible = false;
+            ExitButton.Visible = false;
+            MinimizeButton.Visible = false;
+            ConnectButton.Visible = false;
+            OrbwalkerButton.Visible = false;
+            AutoAimButton.Visible = false;
+            HealBarrierButton.Visible = false;
+            MiscButton.Visible = false;
+            DiscordPic.Visible = false;
+            PayPalPic.Visible = false; 
+            this.TopMost = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ControlBox = false;
+            this.Name = "VoidSharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Update();
+        }
+        private void Update()
+        {
+            this.Show();
+            progressBar1.Value = 3;
+            loadinglabel.Location = new Point(loadinglabel.Left - 20, loadinglabel.Top);
+            loadinglabel.Text = "Checking for Update..";
+            Wait(1500);
+            WebClient webClient = new WebClient();
+            try
+            {
+                if (!webClient.DownloadString("https://pastebin.com/fMU8eZAv").Contains("Update 1.0")) //Github udaje 
+                {
+                    if (MessageBox.Show("An update is available", "Updater", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                    {
+                        Process.Start(new ProcessStartInfo("http://81.162.196.29/downlaod/Void.zip") { UseShellExecute = true });
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Try to restart Void", "Error 7", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Wait(1000);
+                Environment.Exit(0);
+            }
+            progressBar1.Value = 64;
+            loadinglabel.Text = "Loading..";
+            loadinglabel.Location = new Point(loadinglabel.Left + 30, loadinglabel.Top);
+            Wait(500);
+            progressBar1.Value = 100;
+            Wait(100);
+            this.Size = new Size(620, 370);
+            this.Location = new Point(this.Location.X - 190, this.Location.Y - 150);
+            progressBar1.Visible = false;
+            loadinglabel.Visible = false;
+            this.BackgroundImage = Properties.Resources.background;
+            LogoPic.Visible = true;
+            ExitButton.Visible = true;
+            MinimizeButton.Visible = true;
+            ConnectButton.Visible = true;
+            OrbwalkerButton.Visible = true;
+            AutoAimButton.Visible = true;
+            HealBarrierButton.Visible = true;
+            MiscButton.Visible = true;
+            DiscordPic.Visible = true;
+            PayPalPic.Visible = true;
+            this.TopMost = true;
+            this.ShowIcon = true;
+            this.ShowInTaskbar = true;
+            this.ControlBox = true;
+
+        }
+        private void Wait(int time)
+        {
+            Thread thread = new Thread(delegate ()
+            {
+                Thread.Sleep(time);
+            });
+            thread.Start();
+            while (thread.IsAlive)
+                Application.DoEvents();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousedownpoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousedownpoint.IsEmpty)
+                return;
+            Form f = sender as Form;
+            f.Location = new Point(f.Location.X + (e.X - mousedownpoint.X), f.Location.Y + (e.Y - mousedownpoint.Y));
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousedownpoint = Point.Empty;
+        }
+
+        private void ExitButton_MouseEnter(object sender, EventArgs e)
+        {
+            ExitButton.BackColor = Color.FromArgb(143, 37, 37);
+        }
+
+        private void ExitButton_MouseLeave(object sender, EventArgs e)
+        {
+            ExitButton.BackColor = Color.FromArgb(127, 37, 37);
+        }
+
+        private void MinimizeButton_MouseEnter(object sender, EventArgs e)
+        {
+            MinimizeButton.BackColor = Color.FromArgb(130, 134, 134);
+        }
+
+        private void MinimizeButton_MouseLeave(object sender, EventArgs e)
+        {
+            MinimizeButton.BackColor = Color.FromArgb(108, 116, 116);
+        }
+
+        private void DiscordPic_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developer: sajmonekk#1565", "Discord", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void PayPalPic_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.paypal.com/paypalme/sajmonekk") { UseShellExecute = true });
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            generaluser1.Visible = true;
+        }
+    }
+}
---
+++ VoidSharp/VoidSharp/generaluser.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace VoidSharp
+{
+    partial class generaluser
+    {
+        /// <summary> 
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód vygenerovaný pomocí Návrháře komponent
+
+        /// <summary> 
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.darkCombobox1 = new ClassicDarkTheme.Dark.DarkCombobox();
+            this.darkLabel1 = new ClassicDarkTheme.Dark.DarkLabel();
+            this.darkCheckbox1 = new ClassicDarkTheme.Dark.DarkCheckbox();
+            this.darkLabel2 = new ClassicDarkTheme.Dark.DarkLabel();
+            this.darkButton1 = new ClassicDarkTheme.Dark.DarkButton();
+            this.darkButton2 = new ClassicDarkTheme.Dark.DarkButton();
+            this.darkLabel3 = new ClassicDarkTheme.Dark.DarkLabel();
+            this.darkPicturebox1 = new ClassicDarkTheme.Dark.DarkPicturebox();
+            ((System.ComponentModel.ISupportInitialize)(this.darkPicturebox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // darkCombobox1
+            // 
+            this.darkCombobox1.Aligment = System.Drawing.StringAlignment.Near;
+            this.darkCombobox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(39)))));
+            this.darkCombobox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.darkCombobox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.darkCombobox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.darkCombobox1.ForeColor = System.Drawing.Color.White;
+            this.darkCombobox1.FormattingEnabled = true;
+            this.darkCombobox1.Location = new System.Drawing.Point(271, 88);
+            this.darkCombobox1.Name = "darkCombobox1";
+            this.darkCombobox1.Size = new System.Drawing.Size(108, 24);
+            this.darkCombobox1.TabIndex = 0;
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.AutoSize = true;
+            this.darkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.darkLabel1.ForeColor = System.Drawing.Color.White;
+            this.darkLabel1.IsLink = false;
+            this.darkLabel1.Link = "";
+            this.darkLabel1.Location = new System.Drawing.Point(271, 26);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(108, 15);
+            this.darkLabel1.TabIndex = 1;
+            this.darkLabel1.Text = "Champion Settings";
+            // 
+            // darkCheckbox1
+            // 
+            this.darkCheckbox1.BackColor = System.Drawing.Color.Transparent;
+            this.darkCheckbox1.BorderThickness = 3;
+            this.darkCheckbox1.CheckboxBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.darkCheckbox1.CheckboxBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(47)))), ((int)(((byte)(53)))));
+            this.darkCheckbox1.CheckboxDownColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(26)))));
+            this.darkCheckbox1.CheckboxHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.darkCheckbox1.CheckboxLeaveColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(39)))));
+            this.darkCheckbox1.CheckboxUpColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.darkCheckbox1.Checked = false;
+            this.darkCheckbox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.darkCheckbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.darkCheckbox1.ForeColor = System.Drawing.Color.White;
+            this.darkCheckbox1.Location = new System.Drawing.Point(271, 58);
+            this.darkCheckbox1.Name = "darkCheckbox1";
+            this.darkCheckbox1.Size = new System.Drawing.Size(24, 24);
+            this.darkCheckbox1.TabIndex = 2;
+            this.darkCheckbox1.Text = "Enable Shortcuts";
+            this.darkCheckbox1.Type = ClassicDarkTheme.Dark.DarkCheckbox.Types.Standart;
+            // 
+            // darkLabel2
+            // 
+            this.darkLabel2.AutoSize = true;
+            this.darkLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.darkLabel2.ForeColor = System.Drawing.Color.White;
+            this.darkLabel2.IsLink = false;
+            this.darkLabel2.Link = "";
+            this.darkLabel2.Location = new System.Drawing.Point(86, 26);
+            this.darkLabel2.Name = "darkLabel2";
+            this.darkLabel2.Size = new System.Drawing.Size(52, 15);
+            this.darkLabel2.TabIndex = 3;
+            this.darkLabel2.Text = "Connect";
+            // 
+            // darkButton1
+            // 
+            this.darkButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.darkButton1.FlatAppearance.BorderSize = 0;
+            this.darkButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.darkButton1.ForeColor = System.Drawing.Color.White;
+            this.darkButton1.Location = new System.Drawing.Point(53, 48);
+            this.darkButton1.MouseBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.darkButton1.MouseDownColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(26)))));
+            this.darkButton1.MouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.darkButton1.MouseLeaveColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.darkButton1.MouseUpColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.darkButton1.Name = "darkButton1";
+            this.darkButton1.Size = new System.Drawing.Size(124, 34);
+            this.darkButton1.TabIndex = 4;
+            this.darkButton1.Text = "Connect to League";
+            this.darkButton1.UseVisualStyleBackColor = true;
+            // 
+            // darkButton2
+            // 
+            this.darkButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.darkButton2.FlatAppearance.BorderSize = 0;
+            this.darkButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.darkButton2.ForeColor = System.Drawing.Color.White;
+            this.darkButton2.Location = new System.Drawing.Point(53, 192);
+            this.darkButton2.MouseBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.darkButton2.MouseDownColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(26)))));
+            this.darkButton2.MouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.darkButton2.MouseLeaveColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.darkButton2.MouseUpColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.darkButton2.Name = "darkButton2";
+            this.darkButton2.Size = new System.Drawing.Size(124, 34);
+            this.darkButton2.TabIndex = 5;
+            this.darkButton2.Text = "Enable Void";
+            this.darkButton2.UseVisualStyleBackColor = true;
+            // 
+            // darkLabel3
+            // 
+            this.darkLabel3.AutoSize = true;
+            this.darkLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.darkLabel3.ForeColor = System.Drawing.Color.White;
+            this.darkLabel3.IsLink = false;
+            this.darkLabel3.Link = "";
+            this.darkLabel3.Location = new System.Drawing.Point(70, 168);
+            this.darkLabel3.Name = "darkLabel3";
+            this.darkLabel3.Size = new System.Drawing.Size(92, 15);
+            this.darkLabel3.TabIndex = 6;
+            this.darkLabel3.Text = "Ingame Settings";
+            // 
+            // darkPicturebox1
+            // 
+            this.darkPicturebox1.Location = new System.Drawing.Point(265, 123);
+            this.darkPicturebox1.Name = "darkPicturebox1";
+            this.darkPicturebox1.Size = new System.Drawing.Size(120, 120);
+            this.darkPicturebox1.TabIndex = 7;
+            this.darkPicturebox1.TabStop = false;
+            // 
+            // generaluser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.darkPicturebox1);
+            this.Controls.Add(this.darkLabel3);
+            this.Controls.Add(this.darkButton2);
+            this.Controls.Add(this.darkButton1);
+            this.Controls.Add(this.darkLabel2);
+            this.Controls.Add(this.darkCheckbox1);
+            this.Controls.Add(this.darkLabel1);
+            this.Controls.Add(this.darkCombobox1);
+            this.Name = "generaluser";
+            this.Size = new System.Drawing.Size(431, 271);
+            ((System.ComponentModel.ISupportInitialize)(this.darkPicturebox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ClassicDarkTheme.Dark.DarkCombobox darkCombobox1;
+        private ClassicDarkTheme.Dark.DarkLabel darkLabel1;
+        private ClassicDarkTheme.Dark.DarkCheckbox darkCheckbox1;
+        private ClassicDarkTheme.Dark.DarkLabel darkLabel2;
+        private ClassicDarkTheme.Dark.DarkButton darkButton1;
+        private ClassicDarkTheme.Dark.DarkButton darkButton2;
+        private ClassicDarkTheme.Dark.DarkLabel darkLabel3;
+        private ClassicDarkTheme.Dark.DarkPicturebox darkPicturebox1;
+    }
+}
